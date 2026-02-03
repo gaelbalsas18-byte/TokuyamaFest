45bd3a29fc643a457df942cb42db572a25fbc291
Gael <gaelbalsas18@gmail.com>
2026-02-03 11:37:59 -0600

    Mauricio5

--- components/sections/Ponentes.tsx
+++ components/sections/Ponentes.tsx
@@ -7,7 +7,7 @@ const ponentes = [
   {
     id: 1,
     nombre: "Dr. Mauricio Madera",
-    imagen: "/Ponentes/TF_FOTO_Mau2.jpg",
+    imagen: "/Ponentes/TF_Foto_Mau2.jpg",
     imagenModal: "/Ponentes/Mauricio.jpg",
     nombreImagen: "/Ponentes/NombreMauricio.png",
     descripcion: 
